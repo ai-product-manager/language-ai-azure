3c659ab9efa5517306dfd76f6455ac170b6a442b
ai-product-manager <alvaro_maravi@hotmail.com>
2025-02-15 00:09:18 -0500

    health and linked entities

---
+++ health.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+
+load_dotenv()
+
+# set values from .venv, language_key and language_endpoint
+language_key = os.getenv("LANGUAGE_KEY")
+language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
+
+client_subscription = TextAnalyticsClient(
+    language_endpoint, AzureKeyCredential(language_key)
+)
+
+#create function to recognize health entities
+def recognize_health_entities_from_list(client, documents):
+    try:
+        result = client.begin_analyze_healthcare_entities(documents, show_stats=True).result()
+        
+        docs = [doc for doc in result if not doc.is_error]
+        for idx, doc in enumerate(docs):
+            print("Document #{} has the following healthcare entities:".format(idx))
+            for entity in doc.entities:
+                print("Entity: {}".format(entity.text))
+                print("\tNormalized Text: {}".format(entity.normalized_text))
+                print("\tCategory: {}".format(entity.category))
+                print("\tSubcategory: {}".format(entity.subcategory))
+                print("\tConfidence Score: {}".format(entity.confidence_score))
+                print("\tOffset: {}".format(entity.offset))
+                print("\tLength: {}".format(entity.length))
+            for relation in doc.entity_relations:
+                print("Relation:")
+                print("\tRelation Type: {}".format(relation.relation_type))
+                for role in relation.roles:
+                    print("\tRole: {} with entity {}".format(role.name, role.entity.text))
+    except HttpResponseError as err:
+        print("Encountered HTTP response error. {}".format(err))
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+        
+documents_sample = ["Paciente necesita ibuprofeno de 50mg para el dolor de cabeza",
+                    "Mujer embarazada con presión 170 sobre 100, administrado metildopa 500 cada 4 horas"]
+
+recognize_health_entities_from_list(client_subscription, documents_sample)
---
+++ linked_entities.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+
+load_dotenv()
+
+# set values from .venv, language_key and language_endpoint
+language_key = os.getenv("LANGUAGE_KEY")
+language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
+
+client_subscription = TextAnalyticsClient(
+    language_endpoint, AzureKeyCredential(language_key)
+)
+
+#create function to recognize linked entities
+def recognize_linked_entities_from_list(client, documents):
+    try:
+        result = client.recognize_linked_entities(documents, language="es")[0]
+        
+        print("Linked Entities:")
+        for entity in result.entities:
+            print("\tName:", entity.name)
+            print("\tURL:", entity.url)
+            print("\tData Source:", entity.data_source)
+            print("\tDataSource Entity Id:", entity.data_source_entity_id)
+            print("\tMatches:")
+            for match in entity.matches:
+                print("\t\t", "Text:", match.text)
+                print("\t\t", "Confidence Score:", match.confidence_score)
+                print("\t\t", "\tOffset:", match.offset)
+                print("\t\t", "\tLength:", match.length)
+    except HttpResponseError as err:
+        print("Encountered HTTP response error. {}".format(err))
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+        
+documents_sample = [
+    "Microsoft fue fundado por Bill Gates y Paul Allen en 1975. La sede de Microsoft se encuentra en Redmond, Washington. En 1975, Bill Gates y Paul Allen fundaron Microsoft en Redmond, Washington. La empresa se trasladó a Seattle, Washington y a Silicon Valley, California."
+]
+
+recognize_linked_entities_from_list(client_subscription, documents_sample)
+        
